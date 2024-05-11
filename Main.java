bc531cc2c0175d5124584bf0848960b947af58a3
S0L15 <felipesolisrpo@gmail.com>
2024-05-10 22:55:21 -0500

    assignmet3

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        name();
+    }
+    static void name(){
+        System.out.println("Assignment 3 by Felipe Solis restrepo");
+    }
+}
